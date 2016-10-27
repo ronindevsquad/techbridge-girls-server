2b3a137a241300ef1eb6bf835e6aa65190117ab2
Philip Vo <philiptranbavo@gmail.com>
2016-10-26 20:53:45 -0700

    Post page finished.

--- client/assets/controllers/postController.js
+++ client/assets/controllers/postController.js
@@ -1,14 +1,31 @@
-app.controller('postController', function ($scope, $location, $routeParams, $cookies, postsFactory) {
+app.controller('postController', function ($scope, $location, $routeParams, $cookies, $http, postsFactory) {
 	$scope.username = $cookies.get('username');
 	if (!$scope.username)
 		$location.url('/');
 
-	// Get post for id:
+	// Get post and items:
+	$scope.items = [];
 	postsFactory.show($routeParams.id, function(data) {
-		if (data.errors)
+		if (data.errors || !data.image)
 			$location.url('/');
-		else
-			$scope.post = data.post;
+		else {
+			$scope.post = data;
+			for (var i = 0; i < data.links.length; i++) {
+				var id = data.links[i].split('/').slice(-1)[0].split('?')[0];
+				$http.get(`http://api.shopstyle.com/api/v2/products/${id}?pid=uid2900-36524230-6`)
+				.then(function(res) {
+					if (!res.data.errorCode) {
+						$scope.items.push({
+							url: data.links[i],
+							name: res.data.unbrandedName,
+							brand: res.data.brand.name,
+							price: res.data.priceLabel,
+							image: res.data.image.sizes.Large.url
+						});						
+					}
+				})
+			}
+		}
 	});
 
 	$scope.update = function() {
@@ -16,16 +33,22 @@ app.controller('postController', function ($scope, $location, $routeParams, $coo
 		$scope.post.links.push($scope.link);
 		postsFactory.update($scope.post, function(data) {
 			console.log('Data is:', data)
-			$scope.post = data.post;			
+			$scope.post = data;			
 		});
 	}
 	$scope.remove_link = function(index) {
 		$scope.post.links.splice(index, 1);
 		postsFactory.update($scope.post, function(data) {
 			console.log('Data is:', data)
-			$scope.post = data.post;			
+			$scope.post = data;			
 		});		
 	}
+	$scope.remove_post = function() {
+		postsFactory.delete($scope.post._id, function(data) {
+			console.log('Data is:', data)
+			$location.url('/')
+		});		
+	}	
 	$scope.logout = function() {
 		$cookies.remove('token');
 		$cookies.remove('username');
