55f94d619493a3a42c543f51cc0b577346e5121b
Elliot Young <elliotsyoung@gmail.com>
2016-12-12 12:41:36 -0800

    fixed css

--- client/assets/controllers/indexController.js
+++ client/assets/controllers/indexController.js
@@ -1,22 +1,23 @@
 app.controller('indexController', function ($scope, $location, $routeParams, $cookies) {
-	function getPayload(token) {
-		var base64Url = token.split('.')[1];
-		var base64 = base64Url.replace('-', '+').replace('_', '/');
-		return JSON.parse(window.atob(base64));
-	}
-
-	// if ($cookies.get('token')) {
+	// function getPayload(token) {
+	// 	var base64Url = token.split('.')[1];
+	// 	var base64 = base64Url.replace('-', '+').replace('_', '/');
+	// 	return JSON.parse(window.atob(base64));
 	// }
-	// else
+	//
+	// console.log("We are in the controller ");
+	// // if ($cookies.get('token')) {
+	// // }
+	// // else
+	// // 	$location.url('/welcome');
+	//
+	// $scope.logout = function() {
+	// 	$cookies.remove('token');
 	// 	$location.url('/welcome');
-
-	$scope.logout = function() {
-		$cookies.remove('token');
-		$location.url('/welcome');
-	}
-
-	$scope.changeUrlTo = function(url){
-		console.log(" /" + url);
-		$location.url('/'+url);
-	}
+	// }
+	//
+	// $scope.changeUrlTo = function(url){
+	// 	console.log(" /" + url);
+	// 	$location.url('/'+url);
+	// }
 });
