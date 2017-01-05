aa4a490330dc2d4056f307e04cbcedb3eda1cda4
Philip Vo <philiptranbavo@gmail.com>
2017-01-04 18:33:34 -0800

    Changed SQL file.

--- evergreendb.sql
+++ evergreendb.sql
@@ -25,6 +25,7 @@ DROP TABLE IF EXISTS `addresses`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `addresses` (
+  `id` binary(16) NOT NULL,
   `street` varchar(45) NOT NULL,
   `street_ext` varchar(45) DEFAULT NULL,
   `city` varchar(45) NOT NULL,
@@ -34,7 +35,8 @@ CREATE TABLE `addresses` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `proposal_id` binary(16) NOT NULL,
-  PRIMARY KEY (`proposal_id`),
+  PRIMARY KEY (`proposal_id`,`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
   CONSTRAINT `fk_addresses_proposals1` FOREIGN KEY (`proposal_id`) REFERENCES `proposals` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -58,17 +60,13 @@ DROP TABLE IF EXISTS `banks`;
 CREATE TABLE `banks` (
   `account` int(11) NOT NULL,
   `routing` int(11) NOT NULL,
-  `maker_id` binary(16) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',
-  `supplier_id` binary(16) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',
-  PRIMARY KEY (`maker_id`,`supplier_id`),
-  UNIQUE KEY `maker_id_UNIQUE` (`maker_id`),
-  UNIQUE KEY `supplier_id_UNIQUE` (`supplier_id`),
+  `user_id` binary(16) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`user_id`),
   UNIQUE KEY `account_UNIQUE` (`account`),
   UNIQUE KEY `routing_UNIQUE` (`routing`),
-  KEY `fk_bankaccount_maker1_idx` (`maker_id`),
-  KEY `fk_bankaccount_supplier1_idx` (`supplier_id`),
-  CONSTRAINT `fk_bankaccount_maker1` FOREIGN KEY (`maker_id`) REFERENCES `makers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_bankaccount_supplier1` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_banks_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -89,22 +87,17 @@ DROP TABLE IF EXISTS `cards`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cards` (
-  `first_name` varchar(45) DEFAULT NULL,
-  `last_name` varchar(45) DEFAULT NULL,
-  `number` int(11) DEFAULT NULL,
-  `ccv` int(11) DEFAULT NULL,
-  `expiration` datetime DEFAULT NULL,
-  `maker_id` binary(16) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',
-  `supplier_id` binary(16) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `number` int(11) NOT NULL,
+  `ccv` int(11) NOT NULL,
+  `expiration` datetime NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`supplier_id`,`maker_id`),
-  UNIQUE KEY `maker_id_UNIQUE` (`maker_id`),
-  UNIQUE KEY `supplier_id_UNIQUE` (`supplier_id`),
-  KEY `fk_creditcards_maker1_idx` (`maker_id`),
-  KEY `fk_creditcards_supplier1_idx` (`supplier_id`),
-  CONSTRAINT `fk_creditcards_maker1` FOREIGN KEY (`maker_id`) REFERENCES `makers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_creditcards_supplier1` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `user_id` binary(16) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `fk_creditcards_supplier1_idx` (`user_id`),
+  CONSTRAINT `fk_cards_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -151,37 +144,6 @@ LOCK TABLES `machine_labor_rates` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `makers`
---
-
-DROP TABLE IF EXISTS `makers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `makers` (
-  `id` binary(16) NOT NULL,
-  `company` varchar(45) NOT NULL,
-  `contact` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `picture` varchar(45) DEFAULT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `makers`
---
-
-LOCK TABLES `makers` WRITE;
-/*!40000 ALTER TABLE `makers` DISABLE KEYS */;
-/*!40000 ALTER TABLE `makers` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `materials`
 --
 
@@ -190,13 +152,13 @@ DROP TABLE IF EXISTS `materials`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `materials` (
   `id` binary(16) NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
   `cost` decimal(12,2) NOT NULL,
   `unit_cost` decimal(12,2) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `offer_id` binary(16) NOT NULL,
-  PRIMARY KEY (`id`,`offer_id`),
+  PRIMARY KEY (`id`),
   KEY `fk_raw_material_offer1_idx` (`offer_id`),
   CONSTRAINT `fk_raw_material_offer1` FOREIGN KEY (`offer_id`) REFERENCES `offers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -220,24 +182,19 @@ DROP TABLE IF EXISTS `messages`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `messages` (
   `id` binary(16) NOT NULL,
-  `message_supplier` varchar(45) DEFAULT NULL,
   `pdf_attach_s` varchar(45) DEFAULT NULL,
-  `jpg_attach_m` varchar(45) DEFAULT NULL,
-  `message_maker` varchar(45) DEFAULT NULL,
   `pdf_attach_m` varchar(45) DEFAULT NULL,
   `jpg_attach_s` varchar(45) DEFAULT NULL,
+  `jpg_attach_m` varchar(45) DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
-  `supplier_id` binary(16) DEFAULT NULL,
-  `maker_id` binary(16) DEFAULT NULL,
+  `user_id` binary(16) NOT NULL,
   `proposal_id` binary(16) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `fk_message_supplier1_idx` (`supplier_id`),
-  KEY `fk_message_maker1_idx` (`maker_id`),
+  KEY `fk_message_supplier1_idx` (`user_id`),
   KEY `fk_message_proposals1_idx` (`proposal_id`),
-  CONSTRAINT `fk_message_supplier1` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_message_maker1` FOREIGN KEY (`maker_id`) REFERENCES `makers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_message_supplier1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_message_proposals1` FOREIGN KEY (`proposal_id`) REFERENCES `proposals` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -266,14 +223,14 @@ CREATE TABLE `offers` (
   `service` decimal(12,2) NOT NULL,
   `total` decimal(12,2) NOT NULL,
   `proposal_id` binary(16) NOT NULL,
-  `supplier_id` binary(16) NOT NULL,
+  `user_id` binary(16) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`,`proposal_id`,`supplier_id`),
+  PRIMARY KEY (`id`),
   KEY `fk_offer_proposals1_idx` (`proposal_id`),
-  KEY `fk_offer_supplier1_idx` (`supplier_id`),
+  KEY `fk_offer_supplier1_idx` (`user_id`),
   CONSTRAINT `fk_offer_proposals1` FOREIGN KEY (`proposal_id`) REFERENCES `proposals` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_offer_supplier1` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_offer_supplier1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -312,30 +269,32 @@ LOCK TABLES `processes` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `processes_has_suppliers`
+-- Table structure for table `processes_has_users`
 --
 
-DROP TABLE IF EXISTS `processes_has_suppliers`;
+DROP TABLE IF EXISTS `processes_has_users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `processes_has_suppliers` (
-  `supplier_id` binary(16) NOT NULL,
+CREATE TABLE `processes_has_users` (
   `processes_process` varchar(45) NOT NULL,
-  PRIMARY KEY (`supplier_id`,`processes_process`),
-  KEY `fk_processes_has_supplier_supplier1_idx` (`supplier_id`),
-  KEY `fk_processes_has_suppliers_processes1_idx` (`processes_process`),
-  CONSTRAINT `fk_processes_has_supplier_supplier1` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_processes_has_suppliers_processes1` FOREIGN KEY (`processes_process`) REFERENCES `processes` (`process`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `users_id` binary(16) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`processes_process`,`users_id`),
+  KEY `fk_processes_has_users_users1_idx` (`users_id`),
+  KEY `fk_processes_has_users_processes1_idx` (`processes_process`),
+  CONSTRAINT `fk_processes_has_users_processes1` FOREIGN KEY (`processes_process`) REFERENCES `processes` (`process`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_processes_has_users_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `processes_has_suppliers`
+-- Dumping data for table `processes_has_users`
 --
 
-LOCK TABLES `processes_has_suppliers` WRITE;
-/*!40000 ALTER TABLE `processes_has_suppliers` DISABLE KEYS */;
-/*!40000 ALTER TABLE `processes_has_suppliers` ENABLE KEYS */;
+LOCK TABLES `processes_has_users` WRITE;
+/*!40000 ALTER TABLE `processes_has_users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `processes_has_users` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -353,13 +312,13 @@ CREATE TABLE `proposals` (
   `completion` datetime NOT NULL,
   `attachment` varchar(45) DEFAULT NULL,
   `nda` varchar(45) DEFAULT NULL,
-  `maker_id` binary(16) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
+  `user_id` binary(16) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `fk_proposals_maker1_idx` (`maker_id`),
-  CONSTRAINT `fk_proposals_maker1` FOREIGN KEY (`maker_id`) REFERENCES `makers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `fk_proposals_users1_idx` (`user_id`),
+  CONSTRAINT `fk_proposals_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -387,10 +346,10 @@ CREATE TABLE `reports` (
   `note` varchar(255) NOT NULL,
   `created_at` varchar(45) NOT NULL,
   `updated_at` varchar(45) NOT NULL,
-  `supplier_id` binary(16) NOT NULL,
-  PRIMARY KEY (`id`,`supplier_id`),
-  KEY `fk_daily_prod_supplier1_idx` (`supplier_id`),
-  CONSTRAINT `fk_daily_prod_supplier1` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `user_id` binary(16) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_daily_prod_supplier1_idx` (`user_id`),
+  CONSTRAINT `fk_daily_prod_supplier1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -431,14 +390,46 @@ LOCK TABLES `reports_has_materials` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `suppliers`
+-- Table structure for table `urls`
 --
 
-DROP TABLE IF EXISTS `suppliers`;
+DROP TABLE IF EXISTS `urls`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `suppliers` (
+CREATE TABLE `urls` (
+  `homepage` varchar(255) DEFAULT NULL,
+  `facebook` varchar(255) DEFAULT NULL,
+  `instagram` varchar(255) DEFAULT NULL,
+  `linkedin` varchar(255) DEFAULT NULL,
+  `twiiter` varchar(255) DEFAULT NULL,
+  `user_id` binary(16) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  UNIQUE KEY `supplier_id_UNIQUE` (`user_id`),
+  KEY `fk_urls_suppliers1_idx` (`user_id`),
+  CONSTRAINT `fk_urls_suppliers1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `urls`
+--
+
+LOCK TABLES `urls` WRITE;
+/*!40000 ALTER TABLE `urls` DISABLE KEYS */;
+/*!40000 ALTER TABLE `urls` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
   `id` binary(16) NOT NULL,
+  `type` tinyint(1) NOT NULL,
   `company` varchar(45) NOT NULL,
   `contact` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
@@ -453,48 +444,12 @@ CREATE TABLE `suppliers` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `suppliers`
+-- Dumping data for table `users`
 --
 
-LOCK TABLES `suppliers` WRITE;
-/*!40000 ALTER TABLE `suppliers` DISABLE KEYS */;
-INSERT INTO `suppliers` VALUES (';¡·°\Ò6\æ‘[ H\Â|2','hjk','hk','jkja@ia.cs','$2a$10$8EPsei0fuVnQXmFCPNlyseK3cAHuKC1UiBcsWuxFKPgy/7Piqh1T6',NULL,'2017-01-03 20:24:30','2017-01-03 20:24:30'),('g?E\Ò5\æ‘[ H\Â|2','Coding Dojo','Philip Vo','philiptranbavo@gmail.com','$2a$10$Ov3nEtNrEWSrHvOyPZYnjOK2zRSNDR1COFClXYvF9ymGBqUhj7k7G',NULL,'2017-01-03 20:18:34','2017-01-03 20:18:34'),('pdJú\Ò6\æ‘[ H\Â|2','mnjk','njk','kl@as.c','$2a$10$MprfvxfuFkDsmg5WAweWJe3fxfBylYuPc1/fAxrQmiTCmticS/Wp6',NULL,'2017-01-03 20:25:59','2017-01-03 20:25:59'),('’‚,ñ\Ò5\æ‘[ H\Â|2','Apply','Steve Jobs','steve@jobs.com','$2a$10$CycikiZJLnTOCiTAWIrzn.NZkqKotnfKH8lf6yibmf9lhRLw9NAGm',NULL,'2017-01-03 20:19:47','2017-01-03 20:19:47'),('Àx†\Ò6\æ‘[ H\Â|2','supplier','supp','as@sa.cp','$2a$10$drOOKUIsf3IBDV19uk3BiOAUMKzxwuqMeJ72eVrphjLRPmXjMOike',NULL,'2017-01-03 20:28:13','2017-01-03 20:28:13'),('öÁË˜\Ò7\æ‘[ H\Â|2','mkl','nljk','nlk@mlk.co','$2a$10$hTwIz1SoFxjdNICkCLeZgergKu43IWmJav8es7wsusFqAO2o1s0aO',NULL,'2017-01-03 20:36:54','2017-01-03 20:36:54');
-/*!40000 ALTER TABLE `suppliers` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `urls`
---
-
-DROP TABLE IF EXISTS `urls`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `urls` (
-  `homepage` varchar(255) DEFAULT NULL,
-  `facebook` varchar(255) DEFAULT NULL,
-  `instagram` varchar(255) DEFAULT NULL,
-  `linkedin` varchar(255) DEFAULT NULL,
-  `twiiter` varchar(255) DEFAULT NULL,
-  `supplier_id` binary(16) DEFAULT NULL,
-  `maker_id` binary(16) DEFAULT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  UNIQUE KEY `supplier_id_UNIQUE` (`supplier_id`),
-  UNIQUE KEY `maker_id_UNIQUE` (`maker_id`),
-  KEY `fk_urls_suppliers1_idx` (`supplier_id`),
-  KEY `fk_urls_makers1_idx` (`maker_id`),
-  CONSTRAINT `fk_urls_suppliers1` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_urls_makers1` FOREIGN KEY (`maker_id`) REFERENCES `makers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `urls`
---
-
-LOCK TABLES `urls` WRITE;
-/*!40000 ALTER TABLE `urls` DISABLE KEYS */;
-/*!40000 ALTER TABLE `urls` ENABLE KEYS */;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -506,4 +461,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-01-04 15:16:37
+-- Dump completed on 2017-01-04 18:33:21
