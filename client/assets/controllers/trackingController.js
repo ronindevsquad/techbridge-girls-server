06f0200527b92e53c7dafdff978287f3c2332e1d
Elliot Young <elliotsyoung@gmail.com>
2017-01-05 18:17:02 -0800

    changed Bamberg GmBH to {{company}} and fixed the href to flag.png

--- client/assets/controllers/trackingController.js
+++ client/assets/controllers/trackingController.js
@@ -1,10 +1,12 @@
 app.controller('trackingController', function ($scope, $location) {
+	console.log("Inside Tracking Controller");
 	if (payload && $scope.type == 0) {
 
 	}
 	else if (payload && $scope.type == 1) {
 
-	}	
+	}
 	else
 		$location.url('/');
+
 });
