25ed30dd1047cae4603e1badb4ef76b87b7f2f5a
Elliot Young <elliotsyoung@gmail.com>
2017-01-07 11:42:31 -0800

    began work on the tracking page. Set up simple JSON response in backend and Angular controller and factory logic in front end

--- client/assets/controllers/trackingController.js
+++ client/assets/controllers/trackingController.js
@@ -1,4 +1,4 @@
-app.controller('trackingController', function ($scope, $location, offersFactory, reportsFactory) {
+app.controller('trackingController', function ($scope, $location, offersFactory) {
 	console.log("Inside Tracking Controller");
 	if (payload && $scope.type == 0) {
 
@@ -8,12 +8,25 @@ app.controller('trackingController', function ($scope, $location, offersFactory,
 	}
 	else
 		$location.url('/');
-// When the document loads, retrieve the offers (that have been accepted) and reports associated with the id logged in
+// When the document loads, retrieve the offers (that have been accepted) and reports associated with the id logged in. The format of the JSON response should be an array of proposals, each having an (accepted) offer property which contains a reports proprty that is an array of report objects. The back end handles this information.
 	offersFactory.index(function(data){
 		if (data.errors){
 			console.log(data.errors);
 		} else{
-			$scope.offers = data.offers
+			console.log(data);
+			$scope.proposals = data.proposals
 		}
 	});
+
+	$scope.percentCompleted = function(key){
+		var cumulativeUnits = 0
+		for(i=0;i<key.accepted_offer.reports.length;i++){
+			cumulativeUnits+=key.accepted_offer.reports[i].units
+		}
+		var numberToReturn = Math.floor(cumulativeUnits/key.quantity*100)
+		console.log(parseInt(numberToReturn));
+		return numberToReturn
+	}
+
+	$scope.test = "34"
 });
