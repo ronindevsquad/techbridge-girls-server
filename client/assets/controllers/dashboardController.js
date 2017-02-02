d3cafb4ca143c9f1406bc39d3b82122c5cfa6e28
Elliot Young <elliotsyoung@gmail.com>
2017-02-01 19:43:46 -0800

    made changes to proposals and dashboard html

--- client/assets/controllers/dashboardController.js
+++ client/assets/controllers/dashboardController.js
@@ -2,6 +2,12 @@ app.controller('dashboardController', function ($scope, $location, offersFactory
 	chartObject.dataset = null
 	if (payload) {
 		$scope.tab = "dashboard";
+		if($scope.type == 0){
+			console.log("type 0");
+		}else{
+			console.log("type 1");
+		}
+
 		offersFactory.getAcceptedOffers(function(data) {
 			if (data.status == 401)
 				$scope.logout();
@@ -10,8 +16,7 @@ app.controller('dashboardController', function ($scope, $location, offersFactory
 			else {
 			}
 		})
-	}
-	else
+	}else
 		$location.url('/');
 	chartObject.template.width = document.getElementById('chart_div').parentElement.offsetWidth - (2 * document.getElementById('chart_div').parentElement.padding);
 	chartObject.drawChart();
