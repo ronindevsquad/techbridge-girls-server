89c11a8455e9ff6d732eec8b9939ab1589bc33e3
Philip Vo <philiptranbavo@gmail.com>
2017-02-06 17:50:04 -0800

    offers_count

--- server/models/proposal.js
+++ server/models/proposal.js
@@ -26,8 +26,8 @@ module.exports = function(jwt_key) {
 					callback({status: 401, message: "Invalid token. Your session is ending, please login again."});
 				else
 					using(getConnection(), connection => {
-						var query = "SELECT p.*, HEX(p.id) AS id, IFNULL(applications, 0) AS applications, IFNULL(leads, 0) AS leads " +
-						"FROM proposals p " +
+						var query = "SELECT p.*, HEX(p.id) AS id, IFNULL(applications, 0) AS applications, IFNULL(leads, 0) AS leads, " +
+						"COUNT(p.id) AS offers_count FROM proposals p " +
 						"LEFT OUTER JOIN offers o ON o.proposal_id = p.id " +
 						"LEFT OUTER JOIN (select proposal_id, COUNT(*) AS applications FROM offers WHERE status = 1) a ON a.proposal_id = p.id " +
 						"LEFT OUTER JOIN (select proposal_id, COUNT(*) AS leads FROM offers WHERE status = 0) l ON l.proposal_id = p.id " +
