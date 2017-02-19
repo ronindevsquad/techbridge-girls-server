b571cad5e3847879a73d877537efa5a9fea0ee1c
George Miranda <george92miranda@gmail.com>
2017-02-18 20:56:55 -0800

    fix group by error

--- server/models/offer.js
+++ server/models/offer.js
@@ -131,7 +131,7 @@ module.exports = function(jwt_key) {
 						"FROM offers o JOIN users u ON o.user_id = u.id " +
 						"JOIN proposals p on p.id = o.proposal_id " +
 						"JOIN labor_costs l on l.proposal_id = o.proposal_id " +
-						"WHERE o.proposal_id = UNHEX(?)"
+						"WHERE o.proposal_id = UNHEX(?) GROUP BY user_id"
 						return connection.query(query, [req.params.proposal_id, req.params.proposal_id]);
 					}), using(getConnection(), connection => {
 						var query = "SELECT HEX(o.user_id) AS user_id, HEX(o.proposal_id) AS proposal_id, o.status, u.company, u.picture AS picture, o.created_at " +
