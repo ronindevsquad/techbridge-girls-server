f7c384cfa61696cc8ed71bc774a1416164f96e0d
Elliot Young <elliotsyoung@gmail.com>
2017-02-22 21:02:34 -0800

    minor styling changes to proposals partial

--- client/assets/controllers/showProposalController.js
+++ client/assets/controllers/showProposalController.js
@@ -1,7 +1,5 @@
 app.controller('showProposalController', function ($scope, $location, $route, $routeParams, $sce, $anchorScroll,
 	proposalsFactory, offersFactory, routesFactory) {
-	$location.hash('greenwrapper')
-	$anchorScroll();
 	if ($scope.id) {
 		proposalsFactory.show($routeParams.id, function(data) {
 			if (data.status == 401)
