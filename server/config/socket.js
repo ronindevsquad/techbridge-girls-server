bc63445fecad6bf48fe7a05bf5d01d69d1a8c633
Philip Vo <philiptranbavo@gmail.com>
2017-03-25 02:25:00 -0700

    Setting up sockets with observables.

--- server/config/socket.js
+++ server/config/socket.js
@@ -1,57 +1,38 @@
-module.exports = function(server) {
+module.exports = function (server) {
 	const io = require("socket.io").listen(server);
 
-	io.sockets.on("connection", function(socket) {			
-		socket.on('subscribe', function(proposal_id) {
-			socket.join(proposal_id);
+	io.sockets.on("connection", function (socket) {
+		socket.on('test', () => {
+			console.log('test received');
+			socket.emit('testing');
 		});
 
-		socket.on('send', function(data) {
+		socket.on('join', data => {
+			for (let i = 0; i < data.lenth; i++)
+				socket.join(data[i].offer_id);
+		});
+
+		socket.on('send', data => {
 			io.to(data.proposal_id).emit('sent', data);
 		});
 
-		socket.on("logout", function() {
+		socket.on("logout", () => {
 			socket.disconnect();
 		});
 
 		//////////////////////////////////////////////////////
-		//										SENT FROM TRUCKERS
-		//////////////////////////////////////////////////////		
-		// socket.on('apply', function(data) {
-		// 	socket.join(data.application_id);
-		// 	socket.broadcast.emit('applied', data);
-		// });
-
-		// socket.on('cancel', function(data) {
-		// 	socket.broadcast.to(data.application_id).emit('cancelled', data);
-		// });
-
-		// socket.on('forfeit', function(data) {
-		// 	socket.broadcast.to(data.application_id).emit('forfeitted', data);
-		// });
-
-		// socket.on('payLeadFee', function(data) {
-		// 	console.log("in connect", data)
-		// 	socket.broadcast.to(data.application_id).emit('paidLeadFee', data);
-		// });
-
-		// socket.on('invoice', function(data) {
-		// 	socket.broadcast.to(data.application_id).emit('invoiced', data);
-		// });
-		
-		//////////////////////////////////////////////////////
 		//										SENT FROM MAKERS
 		//////////////////////////////////////////////////////			
-		socket.on('accept', function(data) {
-			socket.join(data.proposal_id)
+		socket.on('accept', data => {
+			socket.join(data.proposal_id);
 			socket.broadcast.emit('accepted', data);
-		});		
-		
-		// socket.on('decline', function(data) {
+		});
+
+		// socket.on('decline', data => {
 		// 	socket.broadcast.to(data.application_id).emit('declined', data);
 		// });
 
-		// socket.on('pay', function(data) {
+		// socket.on('pay', data => {
 		// 	socket.broadcast.to(data.application_id).emit('paid', data);
 		// });
 
