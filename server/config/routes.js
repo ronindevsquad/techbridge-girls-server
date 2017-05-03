aeddbb8adc4d49ef40c6fb48ae5cba77511bd9a6
George Miranda <George Miranda>
2017-05-03 10:13:02 -0700

    payments?

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,4 +2,5 @@ const techbridge = require('../controllers/techbridge-girls');
 
 module.exports = function (app) {
 	app.get('/request-signature', techbridge.requestSignature);
+	app.post('/donate', techbridge.donate);
 }
